3bd0b344aee32a15f2af467ced94cbbae2874135
Sagunthala A <117196512+Sagunthala-A@users.noreply.github.com>
2024-01-08 06:23:16 +0000

    delebrately makes error

--- src/components/ButtonClick.js
+++ src/components/ButtonClick.js
@@ -5,8 +5,7 @@ const ButtonClick = ()=> {
     const [clicked,setClicked] = useState(0)
     return (
         <div>
-            <p>Button clicked {clicked} times</p>
-            {/* <h1>sd</h1> */}
+            <p>Button clicked times</p>
             <button onClick={()=>{setClicked(clicked+1)}} >Click me</button>
         </div>
     )
