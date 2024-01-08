e10f13c53b863841bd43d08991e5a24918ad4383
Sagunthala A <117196512+Sagunthala-A@users.noreply.github.com>
2024-01-08 06:17:40 +0000

    done the assignment

---
+++ src/components/ButtonClick.js
@@ -0,0 +1,14 @@
+import React,{useState} from 'react';
+
+const ButtonClick = ()=> {
+
+    const [clicked,setClicked] = useState(0)
+    return (
+        <div>
+            <p>Button clicked {clicked} times</p>
+            <button onClick={()=>{setClicked(clicked+1)}} >Click me</button>
+        </div>
+    )
+}
+
+export default ButtonClick;
